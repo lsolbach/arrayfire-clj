b8c6293ac18ec959da79020558ead5d373547425
Ludger Solbach <soulman@soulspace.org>
2026-01-12 12:13:32 +0100

    add dtype-next dependency

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject arrayfire-clj "0.1.0-SNAPSHOT"
   :description "Clojure wrapper for the ArrayFire tensor math library"
   :dependencies [[org.clojure/clojure "1.12.3"]
-                 [org.suskalo/coffi "1.0.615"]]
+                 [org.suskalo/coffi "1.0.615"]
+                 [cnuernber/dtype-next "11.004"]]
   :source-paths ["src"]
   :jvm-opts ["-Djava.library.path=/opt/arrayfire/lib64"  ; Adjust path as needed
              "--enable-native-access=ALL-UNNAMED"]
