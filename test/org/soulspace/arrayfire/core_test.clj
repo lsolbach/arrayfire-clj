8d7989791d37101c132922f9a565b3e239ba9344
Ludger Solbach <soulman@soulspace.org>
2026-01-12 23:59:12 +0100

    add tests

--- test/org/soulspace/arrayfire/core_test.clj
+++ test/org/soulspace/arrayfire/core_test.clj
@@ -108,3 +108,118 @@
   (run-tests)
   ;
   )
+
+;; Addition tests for all data types
+
+(deftest test-add-float
+  (testing "Addition of float32 arrays"
+    (af/init!)
+    (let [a (af/create-array-f32 [1.0 2.0 3.0] [3])
+          b (af/create-array-f32 [4.0 5.0 6.0] [3])
+          c (af/add a b)
+          out (af/to-host-f32 c 3)]
+      (is (= [5.0 7.0 9.0] out))
+      (af/release a)
+      (af/release b)
+      (af/release c))))
+
+
+(deftest test-add-double
+  (testing "Addition of float64 arrays"
+    (af/init!)
+    (let [a (af/create-array-f64 [1.5 2.5 3.5] [3])
+          b (af/create-array-f64 [4.5 5.5 6.5] [3])
+          c (af/add a b)
+          out (af/to-host-f64 c 3)]
+      (is (= [6.0 8.0 10.0] out))
+      (af/release a)
+      (af/release b)
+      (af/release c))))
+
+
+(deftest test-add-complex-float
+  (testing "Addition of complex32 arrays"
+    (af/init!)
+    (let [a (af/create-array-c32 [[1.0 2.0] [3.0 4.0]] [2])
+          b (af/create-array-c32 [[5.0 6.0] [7.0 8.0]] [2])
+          c (af/add a b)
+          out (af/to-host-c32 c 2)]
+      ;; (1+2i) + (5+6i) = (6+8i)
+      ;; (3+4i) + (7+8i) = (10+12i)
+      (is (= [[6.0 8.0] [10.0 12.0]] out))
+      (af/release a)
+      (af/release b)
+      (af/release c))))
+
+
+(deftest test-add-complex-double
+  (testing "Addition of complex64 arrays"
+    (af/init!)
+    (let [a (af/create-array-c64 [[1.5 2.5] [3.5 4.5]] [2])
+          b (af/create-array-c64 [[5.5 6.5] [7.5 8.5]] [2])
+          c (af/add a b)
+          out (af/to-host-c64 c 2)]
+      ;; (1.5+2.5i) + (5.5+6.5i) = (7.0+9.0i)
+      ;; (3.5+4.5i) + (7.5+8.5i) = (11.0+13.0i)
+      (is (= [[7.0 9.0] [11.0 13.0]] out))
+      (af/release a)
+      (af/release b)
+      (af/release c))))
+
+
+(deftest test-add-int32
+  (testing "Addition of int32 arrays"
+    (af/init!)
+    (let [a (af/create-array-s32 [10 20 30] [3])
+          b (af/create-array-s32 [5 15 25] [3])
+          c (af/add a b)
+          out (af/to-host-s32 c 3)]
+      (is (= [15 35 55] out))
+      (af/release a)
+      (af/release b)
+      (af/release c))))
+
+
+(deftest test-add-uint32
+  (testing "Addition of uint32 arrays"
+    (af/init!)
+    (let [a (af/create-array-u32 [100 200 300] [3])
+          b (af/create-array-u32 [50 150 250] [3])
+          c (af/add a b)
+          out (af/to-host-u32 c 3)]
+      (is (= [150 350 550] out))
+      (af/release a)
+      (af/release b)
+      (af/release c))))
+
+
+(deftest test-add-int64
+  (testing "Addition of int64 arrays"
+    (af/init!)
+    (let [a (af/create-array-s64 [1000 2000 3000] [3])
+          b (af/create-array-s64 [500 1500 2500] [3])
+          c (af/add a b)
+          out (af/to-host-s64 c 3)]
+      (is (= [1500 3500 5500] out))
+      (af/release a)
+      (af/release b)
+      (af/release c))))
+
+
+(deftest test-add-uint64
+  (testing "Addition of uint64 arrays"
+    (af/init!)
+    (let [a (af/create-array-u64 [10000 20000 30000] [3])
+          b (af/create-array-u64 [5000 15000 25000] [3])
+          c (af/add a b)
+          out (af/to-host-u64 c 3)]
+      (is (= [15000 35000 55000] out))
+      (af/release a)
+      (af/release b)
+      (af/release c))))
+
+(comment
+  ;; To run tests from REPL
+  (run-tests)
+  ;
+  )
--- test/org/soulspace/arrayfire/ffi_test.clj
+++ test/org/soulspace/arrayfire/ffi_test.clj
@@ -1,7 +1,8 @@
 (ns org.soulspace.arrayfire.ffi-test
   "Tests for FFI declarations - validates syntax without requiring ArrayFire library"
-  (:require [clojure.test :refer :all]
-            [coffi.mem :as mem]))
+  (:require [clojure.test :refer [deftest is run-tests testing]]
+            [coffi.mem :as mem]
+            [org.soulspace.arrayfire.ffi :as ffi]))
 
 (deftest test-ffi-syntax-validation
   (testing "FFI namespace can be loaded (syntax is valid)"
@@ -33,15 +34,88 @@
     (is (= 8 (mem/size-of ::mem/double)) "double type should be 8 bytes")
     (is (some? (mem/size-of ::mem/pointer)) "pointer type should have a size")))
 
-(deftest test-helper-functions-without-library
-  (testing "Helper functions can be tested without library"
-    ;; We can test pure Clojure functions even without ArrayFire
-    (when (try
-            (require '[org.soulspace.arrayfire.ffi :as ffi])
-            true
-            (catch Exception e false))
-      ;; Only test if namespace loaded
-      (is true "FFI namespace loaded successfully"))))
+(deftest test-memory-operations
+  (testing "Float read/write operations"
+    (let [buf (mem/alloc 4)]
+      (ffi/write-float! buf 0 3.14)
+      (is (< (Math/abs (- 3.14 (ffi/read-float buf 0))) 0.001) "Float roundtrip")))
+  
+  (testing "Double read/write operations"
+    (let [buf (mem/alloc 8)]
+      (ffi/write-double! buf 0 2.71828)
+      (is (< (Math/abs (- 2.71828 (ffi/read-double buf 0))) 0.00001) "Double roundtrip")))
+  
+  (testing "Int read/write operations"
+    (let [buf (mem/alloc 4)]
+      (ffi/write-int! buf 0 42)
+      (is (= 42 (ffi/read-int buf 0)) "Int roundtrip")))
+  
+  (testing "Long read/write operations"
+    (let [buf (mem/alloc 8)]
+      (ffi/write-long! buf 0 123456789)
+      (is (= 123456789 (ffi/read-long buf 0)) "Long roundtrip")))
+  
+  (testing "Short read/write operations"
+    (let [buf (mem/alloc 2)]
+      (ffi/write-short! buf 0 1000)
+      (is (= 1000 (ffi/read-short buf 0)) "Short roundtrip")))
+  
+  (testing "Byte read/write operations"
+    (let [buf (mem/alloc 1)]
+      (ffi/write-byte! buf 0 127)
+      (is (= 127 (ffi/read-byte buf 0)) "Byte roundtrip")))
+  
+  (testing "Complex float read/write operations"
+    (let [buf (mem/alloc 8)]
+      (ffi/write-complex-float! buf 0 [1.5 2.5])
+      (let [[r i] (ffi/read-complex-float buf 0)]
+        (is (< (Math/abs (- 1.5 r)) 0.001) "Complex float real part")
+        (is (< (Math/abs (- 2.5 i)) 0.001) "Complex float imag part"))))
+  
+  (testing "Complex double read/write operations"
+    (let [buf (mem/alloc 16)]
+      (ffi/write-complex-double! buf 0 [3.14 2.71])
+      (let [[r i] (ffi/read-complex-double buf 0)]
+        (is (< (Math/abs (- 3.14 r)) 0.00001) "Complex double real part")
+        (is (< (Math/abs (- 2.71 i)) 0.00001) "Complex double imag part")))))
+
+(deftest test-dims-conversion
+  (testing "dims->native converts dimensions correctly"
+    (let [dims [2 3 4]
+          native-dims (ffi/dims->native dims)]
+      ;; We can't directly read the values without native interop
+      ;; but we can verify it doesn't throw and returns a segment
+      (is (some? native-dims) "dims->native returns a memory segment"))))
+
+(deftest test-type-sizes
+  (testing "Type sizes map has all ArrayFire dtypes"
+    (is (= 4 (get ffi/type-sizes ffi/AF_DTYPE_F32)) "float32 is 4 bytes")
+    (is (= 8 (get ffi/type-sizes ffi/AF_DTYPE_F64)) "float64 is 8 bytes")
+    (is (= 8 (get ffi/type-sizes ffi/AF_DTYPE_C32)) "complex32 is 8 bytes")
+    (is (= 16 (get ffi/type-sizes ffi/AF_DTYPE_C64)) "complex64 is 16 bytes")
+    (is (= 1 (get ffi/type-sizes ffi/AF_DTYPE_B8)) "bool is 1 byte")
+    (is (= 4 (get ffi/type-sizes ffi/AF_DTYPE_S32)) "int32 is 4 bytes")
+    (is (= 4 (get ffi/type-sizes ffi/AF_DTYPE_U32)) "uint32 is 4 bytes")
+    (is (= 8 (get ffi/type-sizes ffi/AF_DTYPE_S64)) "int64 is 8 bytes")
+    (is (= 8 (get ffi/type-sizes ffi/AF_DTYPE_U64)) "uint64 is 8 bytes")
+    (is (= 2 (get ffi/type-sizes ffi/AF_DTYPE_S16)) "int16 is 2 bytes")
+    (is (= 2 (get ffi/type-sizes ffi/AF_DTYPE_U16)) "uint16 is 2 bytes")
+    (is (= 1 (get ffi/type-sizes ffi/AF_DTYPE_U8)) "uint8 is 1 byte")))
+
+(deftest test-arrayfire-dtype-constants
+  (testing "ArrayFire dtype constants are defined"
+    (is (= 0 ffi/AF_DTYPE_F32))
+    (is (= 1 ffi/AF_DTYPE_C32))
+    (is (= 2 ffi/AF_DTYPE_F64))
+    (is (= 3 ffi/AF_DTYPE_C64))
+    (is (= 4 ffi/AF_DTYPE_B8))
+    (is (= 5 ffi/AF_DTYPE_S32))
+    (is (= 6 ffi/AF_DTYPE_U32))
+    (is (= 7 ffi/AF_DTYPE_U8))
+    (is (= 8 ffi/AF_DTYPE_S64))
+    (is (= 9 ffi/AF_DTYPE_U64))
+    (is (= 10 ffi/AF_DTYPE_S16))
+    (is (= 11 ffi/AF_DTYPE_U16))))
 
 (comment
   ;; Manual test to check if ArrayFire is installed
