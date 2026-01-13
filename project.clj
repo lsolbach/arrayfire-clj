ad390ea4fd459a4589f89f53e7c6e49d6bd9175f
Ludger Solbach <soulman@soulspace.org>
2026-01-13 08:56:44 +0100

    added tech.resource dependency

--- project.clj
+++ project.clj
@@ -2,7 +2,9 @@
   :description "Clojure wrapper for the ArrayFire tensor math library"
   :dependencies [[org.clojure/clojure "1.12.3"]
                  [org.suskalo/coffi "1.0.615"]
-                 [cnuernber/dtype-next "11.004"]]
+                 [cnuernber/dtype-next "11.004"]
+                 [techascent/tech.resource "5.09"] ; generalized resource management
+                 ]
   :source-paths ["src"]
   :jvm-opts ["-Djava.library.path=/opt/arrayfire/lib64"  ; Adjust path as needed
              "--enable-native-access=ALL-UNNAMED"]
