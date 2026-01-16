036023a56ed43059cdab67eea88f89f7a4d7896c
Ludger Solbach <soulman@soulspace.org>
2026-01-16 09:44:53 +0100

    add scm and clojars config

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject arrayfire-clj "0.1.0-SNAPSHOT"
+(defproject org.soulspace/arrayfire-clj "0.1.0-SNAPSHOT"
   :description "Clojure wrapper for the ArrayFire tensor math library"
   :dependencies [[org.clojure/clojure "1.12.3"]
                  [org.suskalo/coffi "1.0.615"]
@@ -6,9 +6,12 @@
                  [techascent/tech.resource "5.09"] ; generalized resource management
                  ]
   :source-paths ["src"]
-  :jvm-opts [  ; Adjust library path as needed
+  :jvm-opts [; Adjust library path as needed
              "-Djava.library.path=/opt/arrayfire/lib64"
              ;"-Djava.library.path=/Program Files/ArrayFire/v3/lib"
              "--enable-native-access=ALL-UNNAMED"]
+  :scm {:name "git" :url "https://github.com/lsolbach/arrayfire-clj"}
+  :deploy-repositories [["clojars" {:sign-releases false :url "https://clojars.org/repo"}]]
+
   ;
   )
