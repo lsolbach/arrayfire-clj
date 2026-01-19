97c26716e3b55b09cfad2501d2a3f70e9925d3fc
Ludger Solbach <ludger.solbach@msg.group>
2026-01-19 18:15:24 +0100

    improve namespace docstrings

--- src/org/soulspace/arrayfire/integration/algorithm.clj
+++ src/org/soulspace/arrayfire/integration/algorithm.clj
@@ -1,1 +1,3 @@
-(ns org.soulspace.arrayfire.integration.algorithm)
+(ns org.soulspace.arrayfire.integration.algorithm
+  "Integration of the ArrayFire algorithm related FFI bindings with the error
+   handling and resource management on the JVM.")
--- src/org/soulspace/arrayfire/integration/array.clj
+++ src/org/soulspace/arrayfire/integration/array.clj
@@ -1,1 +1,11 @@
-(ns org.soulspace.arrayfire.integration.array)
+(ns org.soulspace.arrayfire.integration.array
+  "Integration of the ArrayFire array related FFI bindings with the error
+   handling and resource management on the JVM."
+  (:require [org.soulspace.arrayfire.ffi.array :as array]
+            [org.soulspace.arrayfire.integration.resource :as resource]
+            [coffi.mem :as mem]))
+
+;;;
+;;; ArrayFire array integration on the JVM
+;;; 
+
