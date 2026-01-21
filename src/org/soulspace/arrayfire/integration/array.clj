aea1b7df08a310d0143f7d5145388ea1f29ce347
Ludger Solbach <soulman@soulspace.org>
2026-01-21 09:57:47 +0100

    add initial constructor (wip)

--- src/org/soulspace/arrayfire/integration/array.clj
+++ src/org/soulspace/arrayfire/integration/array.clj
@@ -2,10 +2,28 @@
   "Integration of the ArrayFire array related FFI bindings with the error
    handling and resource management on the JVM."
   (:require [org.soulspace.arrayfire.ffi.array :as array]
-            [org.soulspace.arrayfire.integration.jvm-integration :as resource]
-            [coffi.mem :as mem]))
+            [org.soulspace.arrayfire.integration.jvm-integration :as int]
+            [coffi.mem :as mem])
+  (:import (org.soulspace.arrayfire.integration.jvm_integration AFArray)))
 
 ;;;
 ;;; ArrayFire array integration on the JVM
 ;;; 
-
+(defn create-array
+  "Create a new ArrayFire array from host data.
+   Owns exactly one af_array reference."
+  ^AFArray
+  [data dims dtype]
+  (let [out      (int/native-af-array-pointer)
+        data-seg (cond
+                   (floats? data) (int/float-array->segment data)
+                   ;; add other primitive cases here
+                   :else (throw (IllegalArgumentException.
+                                 "Unsupported data type")))
+        dims-seg (int/dims->segment dims)]
+    (int/check!
+     (array/af-create-array out data-seg (count dims)
+      dims-seg dtype)
+     "af-create-array")
+    ;; af_create_array returns a NEW array (refcount = 1)
+    (int/af-array-new (int/deref-af-array out))))
