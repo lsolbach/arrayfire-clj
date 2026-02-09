b5cde2ac532dadba8785eaa34f2e8a1711895e24
Ludger Solbach <soulman@soulspace.org>
2026-02-09 11:54:50 +0100

    update introduction notebook

--- notebooks/introduction.clj
+++ notebooks/introduction.clj
@@ -7,37 +7,61 @@
             [coffi.mem :as mem]))
 
 ;; # Introduction to ArrayFire-CLJ
-;; This notebook provides an introduction to using the ArrayFire-CLJ library,
-;; a Clojure wrapper for the ArrayFire tensor math library. We will cover the
+;; This notebook provides an introduction to using the [ArrayFire-CLJ](https://github.com/lsolbach/arrayfire-clj) library,
+;; a Clojure wrapper for the [ArrayFire](https://github.com/arrayfire/arrayfire) tensor math library. We will cover the
 ;; basics of initializing the library, creating and manipulating arrays, and
 ;; managing resources.
 ;;
 ;; ## ArrayFire
-;; ArrayFire is a high-performance library for parallel computing with a focus
-;; on array operations. It supports multiple backends, including CUDA, OpenCL,
-;; and CPU, allowing you to leverage the power of GPUs and multi-core CPUs for
-;; your computations.
+;; [ArrayFire](https://github.com/arrayfire/arrayfire) is a general-purpose
+;; tensor library that simplifies the software development process for the
+;; parallel architectures found in CPUs, GPUs, and other hardware
+;; acceleration devices.
+;; 
+;; ArrayFire provides:
+;; * Hundreds of functions in the following categories
+;;   * Array handling
+;;   * Computer vision
+;;   * Image processing
+;;   * Linear algebra
+;;   * Machine learning
+;;   * Standard math
+;;   * Signal Processing
+;;   * Statistics
+;;   * Vector algorithms
+;; * Cross-platform compatibility with support for CUDA, oneAPI, OpenCL,
+;;   and native CPU on Windows, Mac, and Linux
 ;;
 ;; ## ArrayFire-CLJ
-;; ArrayFire-CLJ is a Clojure wrapper around the ArrayFire library, providing
-;; a more idiomatic Clojure interface for working with ArrayFire. It abstracts
-;; away the complexities of the underlying C API and allows you to work with
-;; ArrayFire arrays using familiar Clojure data structures and functions.
-;; The library also includes integration tests to ensure that the core functionality
-;; works correctly across different platforms and configurations.
-;; In this notebook, we will explore the core features of ArrayFire-CLJ and how to
-;; use it effectively in your Clojure projects. We will cover device initialization,
-;; array creation and manipulation, memory management, and error handling. By the end
-;; of this notebook, you should have a solid understanding of how to use ArrayFire-CLJ
-;; to perform high-performance computations in Clojure.
+;; [ArrayFire-CLJ](https://github.com/lsolbach/arrayfire-clj) is a Clojure
+;; wrapper around the ArrayFire library, providing a more idiomatic Clojure
+;; interface for working with ArrayFire. It abstracts away the complexities of
+;; the underlying C API and allows you to work with ArrayFire arrays using
+;; familiar Clojure data structures and functions.
+;;
+;; The library also includes integration tests to ensure that the core
+;; functionality works correctly across different platforms and configurations.
+;;
+;; In this notebook, we will explore the core features of ArrayFire-CLJ and how
+;; to use it effectively in your Clojure projects. We will cover device
+;; initialization, array creation and manipulation, memory management,
+;; and error handling. By the end of this notebook, you should have a solid
+;; understanding of how to use ArrayFire-CLJ to perform high-performance
+;; computations in Clojure.
 ;;
 ;; ## Initialization
-;; Before using ArrayFire, we need to initialize the library and select a device. The `device` namespace provides functions for this purpose.
+;; Before using ArrayFire, we need to initialize the library and select a
+;; device. The `device` namespace provides functions for this purpose.
+
 (device/init!)
-;; This initializes ArrayFire and selects the default device. You can also specify a backend (e.g., CUDA, OpenCL) if you have multiple backends available.
+
+;; This initializes ArrayFire and selects the default device. You can also
+;; specify a backend (e.g., CUDA, OpenCL) if you have multiple backends
+;; available.
 ;;
 ;; ## Device Information
 ;; We can query information about the available devices and their capabilities.
+
 (let [device-info (device/info-string)
       device-count (device/get-device-count)
       current-device (device/get-device)
@@ -47,10 +71,12 @@
   (println "Device Count:" device-count)
   (println "Current Device ID:" current-device)
   (println "Device Info:\n" device-info)
-  (println "Memory Info:" mem-info)
   (println "Double Precision Support:" dbl-support)
-  (println "Half Precision Support:" half-support))
+  (println "Half Precision Support:" half-support)
+  (println "Memory Info:" mem-info))
 
 ;; ## Array Creation and Manipulation
-;; The `array` namespace provides functions for creating and manipulating ArrayFire arrays. We can create arrays from Clojure data structures or directly from memory.
+;; The `array` namespace provides functions for creating and manipulating
+;; ArrayFire arrays. We can create arrays from Clojure data structures or
+;; directly from memory.
 
