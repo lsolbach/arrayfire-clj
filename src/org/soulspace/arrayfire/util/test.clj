80b0b60f02bd90edcaeabf72250fddb02745dd30
Ludger Solbach <soulman@soulspace.org>
2026-02-22 21:13:52 +0100

    add seq-approx= to test util

--- src/org/soulspace/arrayfire/util/test.clj
+++ src/org/soulspace/arrayfire/util/test.clj
@@ -8,3 +8,10 @@
    (<= (Math/abs (- (double expected) (double actual)))
        (double tolerance))))
 
+(defn seq-approx=
+  "Returns true when every corresponding pair of elements in flat sequences xs
+   and ys is approximately equal within tol."
+  ([xs ys]     (seq-approx= xs ys 1e-5))
+  ([xs ys tol] (and (= (count xs) (count ys))
+                    (every? true? (map #(approx= %1 %2 tol) xs ys)))))
+
