d7d244ece01f9d73cacb0d7ccd77380710172c1b
Ludger Solbach <soulman@soulspace.org>
2026-02-24 01:20:57 +0100

    update docstring of stub

--- src/org/soulspace/arrayfire/api/computer_vision.clj
+++ src/org/soulspace/arrayfire/api/computer_vision.clj
@@ -1,8 +1,11 @@
 (ns org.soulspace.arrayfire.api.computer-vision
   "Idiomatic Clojure API for ArrayFire's computer vision functions.
    
-   This namespace provides high-level, user-friendly functions that wrap the lower-level ArrayFire computer vision API.
-   It includes functions from the vision and features Unified API namespaces."
+   This namespace provides high-level, user-friendly functions that wrap the
+   lower-level ArrayFire computer vision API.
+   
+   It includes functions from the org.soulspace.arrayfire.integration.unified-api.vision
+   and org.soulspace.arrayfire.integration.unified-api.features namespaces."
   (:require [org.soulspace.arrayfire.integration.base.definitions :as defs]
             [org.soulspace.arrayfire.integration.unified-api.features :as features]
             [org.soulspace.arrayfire.integration.unified-api.vision :as vision]
